6ef8805630907ed1f4fa054e057be099b6e20e4a
Muhammad Fariz Luthfi <84334672+Ziraf420@users.noreply.github.com>
2023-05-05 22:08:30 +0700

    u

--- daftar_artikel.js
+++ daftar_artikel.js
@@ -1,25 +1,158 @@
-const judul = ["Pengertian ADHD (Attention Deficit Hyperactivity Disorder)","Ciri - ciri ADHD (Attention Deficit Hyperactivity Disorder)","Jika ADHD (Attention Deficit Hyperactivity Disorder) terlambat ditangani"];
+const judul = ["ADHD, Gangguan Mental yang Sering Dialami Anak Milenial","Jangan Keliru! Kenali perbedaan ADHD dengan Autisme pada anak","4 Tips untuk Orang Tua Agar Anak dengan ADHD Memiliki Motivasi dan Fokus"];
 const isi = [
-	`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+	`Tidak hanya pada orang dewasa, nyatanya gangguan kesehatan mental juga dapat 
+     dialami oleh anak-anak yang akan beranjak dewasa. Berbagai gangguan kesehatan 
+     sangat rentan dialami oleh anak-anak yang masuk kategori milenial, misalnya 
+     gangguan kecemasan, depresi, hingga alami ADHD. ADHD atau yang dikenal juga 
+     dengan deficit hyperactivity disorder adalah gangguan mental yang menyebabkan 
+     pengidapnya kesulitan memusatkan perhatian serta memiliki perilaku impulsif dan 
+     juga hiperaktif.</br></br>
      
-	`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+     Tidak ada salahnya bagi para orangtua yang memiliki anak usia remaja, kenali 
+     lebih banyak gangguan kesehatan mental, seperti ADHD agar ibu dapat mengenali 
+     gejala yang dialami oleh para remaja serta mengatasi kondisi ini dengan tepat. 
+     ADHD yang tidak diatasi dengan baik nyatanya dapat menurunkan kualitas hidup 
+     pengidapnya serta memengaruhi kondisi prestasi akademis di sekolah.</br></br>
+     
+     <h2><b>Kenali Gejala ADHD pada Remaja</b></h2></br>
 
-	`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+     Melansir dari Organisasi Kesehatan Dunia (WHO), 10–20 persen remaja nyatanya 
+     pernah mengalami kondisi gangguan mental, baik yang disadari maupun tidak. 
+     Salah satu gangguan mental yang kerap dialami oleh para remaja yang masuk 
+     dalam kategori anak milenial adalah ADHD atau yang dikenal juga dengan deficit 
+     hyperactivity disorder.</br></br>
+     
+     Saat mengalami ADHD biasanya pengidap akan kesulitan untuk memusatkan perhatian, 
+     serta memengaruhi perilakunya yang menjadi impulsif serta hiperaktif. Melansir 
+     Children and Adults with Attention –Deficit/Hyperactivity Disorder, beberapa 
+     remaja tidak menyadari kondisi ADHD yang dialami karena tidak menunjukkan gejala 
+     pada awalnya.</br></br>
+     
+     Biasanya, gejala akan disadari ketika kondisi gangguan mental sudah cukup parah saat 
+     pengidap beranjak remaja. Ada beberapa gejala yang menjadi tanda adanya kondisi ADHD, 
+     seperti sulit berkonsentrasi dan mendengarkan orang lain, perhatian mudah teralihkan, 
+     tidak memerhatikan hal kecil, tidak dapat duduk tenang, sulit mengatur tugas atau kegiatan 
+     yang sedang dilakukan, terlalu banyak bicara, mengganggu aktivitas yang dilakukan orang lain, 
+     serta kebiasaan menggerakan beberapa bagian tubuh saat sedang duduk.</br></br>
+
+     Seiring bertambahnya usia, pengidap ADHD akan mengalami gangguan pada pendidikan maupun 
+     pekerjaannya. Selain itu, pengidap ADHD akan kesulitan memiliki teman atau pasangan hidup. 
+     Jangan ragu segera lakukan pemeriksaan pada rumah sakit terdekat ketika kamu atau kerabat 
+     dekat yang mengalami beberapa gejala terkait dengan kondisi ADHD.</br></br>
+     
+     <h2><b>Pemeriksaan bagi Pengidap ADHD</b></h2></br>
+     
+     Ada beberapa faktor yang dapat menyebabkan seseorang alami kondisi ADHD saat remaja, seperti 
+     memiliki riwayat keluarga dengan kondisi ADHD, mengalami kelahiran prematur sebelum usia kehamilan 
+     37 minggu yang menyebabkan pertumbuhan otak dalam kandungan belum optimal, adanya kelainan pada 
+     struktur otak dalam kandungan, penggunaan obat terlarang saat ibu pengidap menjalani masa kehamilan, 
+     paparan racun atau bahan kimia dalam waktu yang cukup panjang ketika masa balita.</br></br>
+     
+     Itulah beberapa faktor pemicu yang dapat menyebabkan seseorang alami ADHD. Namun perlu diketahui, 
+     tidak semua anak yang sulit berkonsentrasi dan aktif merupakan tanda dari ADHD. Untuk memastikan kondisi 
+     ini, sebaiknya kunjungi rumah sakit terdekat dan lakukan pemeriksaan yang bisa mendeteksi adanya kondisi 
+     ADHD pada seseorang.Pemeriksaan akan dilakukan dengan wawancara dan pemeriksaan fisik yang kemudian dilanjutkan 
+     dengan tes darah, tes fungsi hati, tes fungsi tiroid, serta MRI otak.</br></br>
+     
+     <h2><b>Penanganan Anak dengan Kondisi ADHD</b></h2></br>
+     
+     Lalu, bagaimana jika dokter mendeteksi adanya kondisi ADHD pada anak? Meski tidak dapat disembuhkan secara optimal, 
+     penanganan terhadap kondisi ADHD berfungsi untuk menurunkan risiko gejala yang akan dialami. Berikut ini perawatan 
+     yang bisa dilakukan pengidap ADHD:</br></br>
+     
+     <h5><b>1. Pengobatan</b></h5>
+
+     Beberapa penggunaan jenis obat-obatan digunakan untuk menurunkan risiko gejala yang akan dialami oleh pengidap ADHD. </br></br>
+     
+     <h5><b>2. Terapi</b></h5>
+
+     Ada beberapa terapi yang bisa dilakukan pengidap ADHD untuk meredakan gejala dan menurunkan risiko munculnya gejala, 
+     seperti terapi perilaku kognitif, terapi psikoedukasi, hingga pelatihan interaksi sosial.</br></br>
+     
+     <h5><b>3. Perubahan Hidup Sehat</b></h5>
+
+     Selain pengobatan yang disarankan oleh dokter, orangtua juga perlu mendukung anak agar penanganan yang dilakukan dapat 
+     berhasil. Menerapkan pola hidup sehat dapat dilakukan orangtua untuk menurunkan risiko gejala yang muncul. Berikan anak 
+     makanan yang bernutrisi tinggi, jangan lupa untuk penuhi kebutuhan istirahat anak setiap harinya, ajak anak untuk rutin 
+     olahraga agar kesehatan fisik ikut terjaga dengan baik.</br></br>
+
+     <h10 style="font-size:12px">Referensi:
+     Children and Adults with Attention –Deficit/Hyperactivity Disorder. Diakses pada 2020. Diagnosing ADHD in Adolescence.</br>
+     Teens Health. Diakses pada 2020. ADHD.</br>
+     World Health Organization. Diakses pada 2020. Adolescent Mental Health.</br>
+     Child Mind Institute. Diakses pada 2020. ADHD in Teenagers.</h10>`,
+     
+	`Sekitar 10% anak sekolah mengalami ADHD, tapi gangguan ini masih sulit dikenali karena tak jarang orang menganggap ADHD 
+     sama dengan autisme. Padahal jelas, keduanya gangguan yang berbeda.Dilansir dari National Institute of Mental Health, ADHD 
+     terjadi pada otak dan ditandai dengan kurangnya perhatian atau hiperaktif yang mengganggu fungsi dan perkembangan otak anak.
+
+     Autisme dan ADHD membuat penderita sulit untuk berkomunikasi dengan bahasa yang baik, meski hampir serupa. Autisme sendiri merupakan 
+     kondisi ketika adanya gangguan pada sistem saraf dan gejalanya terlihat pada sejak tahun kedua mereka dilahirkan. Dua jenis gangguan 
+     mental ini sama sama memiliki masalah terkait perhatian, perilaku yang tiba-tiba berubah dan sulit berhubungan dengan orang lain. 
+     Lalu apa bedanya? Jika diperhatikan, ADHD lebih memengaruhi bagaimana anak tumbuh dan berkembang. Sedangkan autisme rangkaian gangguan 
+     perkembangan yang memengaruhi perilaku, interaksi sosial, dan kemampuan bahasa serta belajar.</br></br>
+
+     <h5><b>1. Dari segi perhatian</b></h5>
+     Anak autisme menghindari kegiatan yang sifatnya terlalu fokus seperti membaca buku, bahkan dari awal sudah terlihat tidak minat dengan
+     hal-hal seperti itu. Sementara itu, anak ADHD fokus pada kegiatan yang membutuhkan fokus dan konsentrasi tinggi. Seperti berhitung.</br></br>
+
+     <h5><b>2. Dari segi rutinitas yang dilakukan</b></h5>
+     Anak autisme senang melakukan kegiatan yang sudah teratur dan tidak menyukai perubahan yang tiba-tiba. Sedangkan, anak ADHD kebalikan 
+     dari autisme. Sebab, mereka cenderung senang dengan aktivitas yang sama dalam waktu yang juga lama.</br></br>
+
+     <h5><b>3. Dari segi interaksi bersama orang lain</b></h5>
+     Anak autisme sering merasa sulit memasukkan kata-kata dalam pikiran mereka, sulit mengungkapkan emosi, dan tidak peka terhadap orang lain. 
+     Sementara, anak ADHD cenderung bicara tanpa henti dan bisa mengganggu saat orang lain berbicara, bahkan dominan dalam pembicaraan.</br></br>
+     
+     <h10 style="font-size:12px">Referensi:
+     Konten ini telah tayang di Kompasiana.com dengan judul "Jangan Keliru! Kenali Perbedaan Autisme dan ADHD pada Anak", </br>Klik untuk baca:
+     https://www.kompasiana.com/arumpusporini/63fdb53c08a8b51e7552d232/jangan-keliru-kenali-perbedaan-autisme-dan-adhd-pada-anak?page=all#section1</br>Kreator: Arum Pusporini.</h10>`,
+
+	`Salah satu masalah utama yang dihadapi oleh anak dengan Attention Deficit Hyperactivity Disorder (ADHD) adalah kesulitan untuk termotivasi 
+     dan fokus. Kesulitan tersebut dapat menghambat tugas anak dengan ADHD di sekolah, rumah, dan lingkungannya lainnya. Seorang anak didiagnosis 
+     ADHD karena ada faktor genetik serta fungsi dan struktur otak, seperti melansir National Health Service (NHS).Dalam keluarga, ADHD pada anak 
+     cenderung diturunkan secara genetik. Pada banyak kasus, diduga gen yang diwariskan oleh orang tua merupakan faktor penting ADHD dapat berkembang
+     pada anak.</br></br>
+     
+     Namun, cara ADHD diwariskan merupakan hal yang kompleks. Oleh karena itu, ADHD tidak dianggap hanya berkaitan dengan genetik. Fungsi dan struktur 
+     otak juga merupakan sebuah faktor penyebab. Penelitian NHS mengungkap sejumlah kemungkinan perbedaan antara otak orang-orang non-ADHD dengan yang 
+     didiagnosis ADHD. Penelitian yang melibatkan pengamatan (scan) otak tersebut menunjukkan, otak orang dengan ADHD memiliki bagian tertentu yang lebih 
+     kecil dibanding bagian lainnya. Adapun anak dengan ADHD yang sulit fokus tidak ada kaitannya dengan rasa malas ataupun kurangnya kecerdasan, melansir 
+     Psych Central. Sebab, otak anak dengan ADHD tidak termotivasi dengan cara yang sama seperti otak anak pada umumnya. Anak dengan ADHD tahu ketika mereka 
+     harus mengerjakan sesuatu. Meski begitu, bagi mereka, keadaan selalu terasa seperti mengharuskan mereka untuk berdiam saja. Namun, ada beberapa hal yang 
+     ampuh untuk mendorong mereka agar bergerak.</br></br>
+     
+     <h5><b>Berikan anak tekanan</b></h5>
+     
+     Tekanan bukan selalu berarti memberikan anak hal yang bisa membuatnya frustrasi. Tekanan yang diberikan bertujuan membuat mereka merasa bahwa mereka tak 
+     memiliki pilihan selain bergerak. Adapun tekanan bisa berupa tekanan interpersonal dan waktu. Dalam tekanan interpersonal, keterlibatan orang lain yang 
+     mengatur anak sangat diperlukan. Tujuannya, agar anak merasa takut untuk mengecewakan atau membuat orang lain tersebut marah. Dengan begitu, peran orang 
+     tua dan guru sangat dibutuhkan di sini. Tak hanya itu, tekanan waktu juga terbukti efektif mengatasi kurangnya motivasi dan fokus. Biasanya, telah diberikan 
+     tenggat waktu (deadline) mengumpulkan tugas dari sekolah atau tempat les. Meski begitu, orang tua dapat memberikan tenggat waktu pribadi kepada anak agar ia 
+     termotivasi untuk segera menyelesaikannya.</br></br>
+
+     <h5><b>Sesuatu yang baru</b></h5>
+
+     Otak manusia dibentuk untuk mengeksplor hal-hal baru, terutama anak dengan ADHD. Bedanya, mereka membutuhkan kebaruan tersebut dalam skala, ukuran, atau jumlah 
+     yang lebih besar. Kebaruan dapat memicu otak anak dengan ADHD untuk terus belajar. Keinginannya untuk eksplor dan belajar membuat otaknya memiliki dopamin dengan 
+     dosis yang sehat. Kemudian, hal ini dapat membuatnya terus bergerak.</br></br>
+
+     <h5><b>Minat atau passion</b></h5>
+     
+     Bagi orang tua, mungkin terasa membingungkan ketika melihat perbedaan semangat anak dengan ADHD yang berbeda ketika melakukan hal-hal yang berbeda. Misalnya, ia dapat 
+     fokus dan termotivasi ketika bermain bola, tetapi fokusnya berubah drastis ketika ia harus mengerjakan PR Matematika. Oleh karena itu, memantau anak dengan ADHD dapat 
+     membantu orang tua memahami apa yang mereka sukai. Sebab, minat atau passion-lah yang menghidupkan otak anak dengan ADHD. Minat membuat otaknya mengarahkannya untuk 
+     bergerak. Meskipun kesulitan untuk mulai sangat mungkin masih terjadi, tetapi tak akan separah ketika ia harus memulai hal yang tak ia sukai.</br></br>
+
+     <h5><b>Persaingan yang sehat</b></h5>
+
+     Persaingan yang sehat dapat memicu otak anak dengan ADHD agar termotivasi dan fokus. Mendorongnya untuk bersaing secara sehat dapat membuatnya bersemangat dan aktif. 
+     Sebab, hal ini dapat membuat ia bertujuan untuk menang daripada memulai. Hal ini terbukti dapat secara efektif menghilangkan kesulitan anak untuk memulai sesuatu. Meski
+     begitu, persaingan tak selalu bisa dijadikan pilihan. Sebab, persaingan yang berlebihan justru bisa menjadi tidak sehat untuk kesehatan mental anak. Dengan begitu, orang 
+     tua tetap perlu bijak dengan mengenal karakter anak sepenuhnya sebelum mendorongnya untuk bersaing.</br></br>
+
+     <h10 style="font-size:12px">Referensi:
+     https://www.liputan6.com/disabilitas/read/5248311/4-tips-untuk-orang-tua-agar-anak-dengan-adhd-memiliki-motivasi-dan-fokus</h10>`
 ]
 
 
